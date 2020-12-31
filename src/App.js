482ee0ccf10386c111516fa1ab9e7850f3be8302
Sakib-Imtiaz <72743224+Sakib-Imtiaz@users.noreply.github.com>
2020-12-31 02:04:15 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Modified by Majd Uddin <br />
+           <br />
           Edit <code>src/App.js</code> and save to reload.
+    <br />  Create by Sakib Imtiaz
         </p>
         <a
           className="App-link"
